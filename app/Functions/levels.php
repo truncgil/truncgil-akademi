fcc1dfc6cf5ab563a89fe6a23da364ee3c5cd40c
Ümit Tunç <umitreva@gmail.com>
2023-03-02 08:03:17 +0300

    level structre farm control system

--- app/Functions/levels.php
+++ app/Functions/levels.php
@@ -1,14 +1,17 @@
 <?php function levels() {
+    // user no, full control, write, read, modify
     return [
-        'Admin',
-        'Center Office',
-        'NDT',
-        'Welder (Subcontractor)',
-        'Painter (Subcontractor)',
-        'Insulator (Subcontractor)',
-        'Insulator (Subcontractor)',
-        'Welder, Painter, Insulator (Subcontractor / Payrollless)',
-        'Quality Staff',
-        'Field Staff',
+    'Admin' =>                          [1,1,1,1,1],
+        'Manager (Center Office)' =>    [2,0,1,1,1],
+        'Manager (QC)' =>               [2,0,1,1,0],
+        'Manager (PTO)' =>              [2,0,1,1,0],
+        'Manager (Lead)' =>             [2,0,1,1,0],
+        'Welder (Subcontractor)' =>     [4,0,1,1,0],
+        'Painter (Subcontractor)' =>    [5,0,1,1,0],
+'Insulator (Subcontractor)' =>          [6,0,1,1,0],
+        'Welder, Painter, Insulator (Subcontractor / Payrollless)' =>   [7,0,1,1,0],
+        'Quality Staff' =>                                              [8,0,1,1,1],
+        'Document Staff' =>                                             [9,0,1,1,1],
+        'Field Staff' =>                                                [10,0,1,1,1],
     ];
 } ?>
