3c0e4d7e721609084a09152f544575142cd4ee3c
Ümit Tunç <umitreva@gmail.com>
2023-03-03 15:54:52 +0300

    add header

---
+++ resources/views/admin/type/naks-technology/header.blade.php
@@ -0,0 +1,34 @@
+<tr>
+    <td>{{e2("SN")}}</td>
+    <td>{{e2("CERTIFICATE No")}}</td>
+    <td>{{e2("Short Name")}}</td>
+    <td>{{e2("Valid from")}}</td>
+    <td>{{e2("Valid to")}}</td>
+    <td>{{e2("Welding Method")}}</td>
+    <td>{{e2("MAT. GROUP 1")}}</td>
+    <td>{{e2("MAT. GROUP 2")}}</td>
+    <td>{{e2("Welding consumables")}}</td>
+    <td>{{e2("CATEGORY")}}</td>
+    <td>{{e2("TYPE")}}</td>
+    <td>{{e2("MIN")}}</td>
+    <td>{{e2("MAX")}}</td>
+    <td>{{e2("MIN2")}}</td>
+    <td>{{e2("MAX2")}}</td>
+    <td>{{e2("MIN THICK")}}</td>
+    <td>{{e2("MAX THICK")}}</td>
+    <td>{{e2("MIN THICK 2")}}</td>
+    <td>{{e2("MAX THICK 2")}}</td>
+    <td>{{e2("JOINT TYPE")}}</td>
+    <td>{{e2("PWHT")}}</td>
+    <td>{{e2("CONNECTION TYPE")}}</td>
+    <td>{{e2("JOINT VIEW")}}</td>
+    <td>{{e2("ANGLE TYPE")}}</td>
+    <td>{{e2("POSITION")}}</td>
+    <td>{{e2("PRE HEATING")}}</td>
+    <td>{{e2("Shielding gas")}}</td>
+    <td>{{e2("Type of electrode coating")}}</td>
+    <td>{{e2("Type of welding equipment")}}</td>
+    <td>{{e2("Performed works type")}}</td>
+    <td>{{e2("Remarks")}}</td>          
+    <td>{{e2("Opt")}}</td>          
+</tr>
