0bbff707de0b7a8aff109108090956b6b149ace7
Ümit Tunç <umitreva@gmail.com>
2023-03-03 19:33:21 +0300

    fix naks center form

--- resources/views/admin/type/settings/naks-center.blade.php
+++ resources/views/admin/type/settings/naks-center.blade.php
@@ -18,30 +18,35 @@ if(getisset("delete-center")) {
 }
         ?>
 <table class="table table-bordered table-sm">
+    <thead>
     <tr>
         <th>{{e2("Center No")}}</th>
         <th>{{e2("Center Name")}}</th>
         <th>{{e2("Opt")}}</th>
     </tr>
-    <form action="?t={{get("t")}}&add-center" method="post">
+        <form action="?t={{get("t")}}&add-center" method="post">
         @csrf
         <tr class="table-warning">
             <td><input type="text" name="center_no" id="" class="form-control"></td>
             <td><input type="text" name="center_name" id="" class="form-control"></td>
             <td><button class="btn btn-default btn-sm"><i class="fa fa-plus"></i></button></td>
         </tr>
+        </form>
+    </thead>
+    <tbody>
         <?php 
-            foreach(NaksCenter::all() AS $naksCenter) {
-                 ?>
-                <tr>
-                    <td><input type="text" name="center_no" value="{{$naksCenter->center_no}}" table="naks_centers" id="{{$naksCenter->id}}" class="form-control edit"></td>
-                    <td><input type="text" name="center_name" value="{{$naksCenter->center_name}}" table="naks_centers" id="{{$naksCenter->id}}" class="form-control edit"></td>
-                    <td>
-                        <a href="?t={{get("t")}}&delete-center={{$naksCenter->id}}" teyit="{{e2("Are you sure you want to delete?")}}" class="btn btn-sm btn-danger"><i class="fa fa-times"></i></a>
-                    </td>
-                </tr>
-                 <?php 
-            }
+        foreach(NaksCenter::all() AS $naksCenter) {
+                ?>
+            <tr>
+                <td><input type="text" name="center_no" value="{{$naksCenter->center_no}}" table="naks_centers" id="{{$naksCenter->id}}" class="form-control edit"></td>
+                <td><input type="text" name="center_name" value="{{$naksCenter->center_name}}" table="naks_centers" id="{{$naksCenter->id}}" class="form-control edit"></td>
+                <td>
+                    <a href="?t={{get("t")}}&delete-center={{$naksCenter->id}}" teyit="{{e2("Are you sure you want to delete?")}}" class="btn btn-sm btn-danger"><i class="fa fa-times"></i></a>
+                </td>
+            </tr>
+                <?php 
+        }
         ?>
-    </form>
+    </tbody>
+    
 </table>
