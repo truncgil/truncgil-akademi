e09f7812a8be5bd60bf56c5f4f6dae7d470e656c
Ümit Tunç <umitreva@gmail.com>
2023-03-07 17:13:12 +0300

    Welding Methods has been created

---
+++ database/migrations/2023_03_07_093508_create_welding_methods_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWeldingMethodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+{
+        Schema::create('welding_methods', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('en_welding_number')->nullable();
+            $table->string('russian_definition')->nullable();
+            $table->string('iso_4063_definition')->nullable();
+            $table->string('aws')->nullable();
+            $table->string('iso_short_name')->nullable();
+            $table->string('aws_short_name')->nullable();
+            $table->string('ru_short_name')->nullable();
+   
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('welding_methods');
+    }
+}
---
+++ resources/views/admin/type/settings/welding-methods.blade.php
@@ -0,0 +1,77 @@
+<?php  
+use App\Models\WeldingMethod; 
+if(getisset("add")) {
+    $post = $_POST;
+    unset($post['_token']);
+    
+    try {
+        WeldingMethod::create($post);
+        bilgi("Naks Center has been created","success");
+    } catch (\Throwable $th) {
+        dump($th);
+    }
+            
+}
+if(getisset("delete")) {
+    WeldingMethod::where('id', get('delete'))->delete();
+
+}
+        ?>
+
+<?php  
+$tableName = "welding_methods"; ?>
+@include("components.excel-buttons")
+@include("components.excel-file-input")
+<div class="table-responsive">
+<table class="table table-bordered table-sm">
+    <thead>
+    <tr>
+        <th data-resizable-column-id="id">{{e2("ID")}}</th>
+        <th data-resizable-column-id="En-Welding-Number">{{e2("En Welding Number")}}</th>
+        <th data-resizable-column-id="Russian_Definition">{{e2("Russian Definition")}}</th>
+        <th data-resizable-column-id="ISO-4063-Definition">{{e2("ISO 4063 Definition")}}</th>
+        <th data-resizable-column-id="AWS">{{e2("AWS")}}</th>
+        <th data-resizable-column-id="ISO Short name">{{e2("ISO Short name")}}</th>
+        <th data-resizable-column-id="AWS Short name">{{e2("AWS Short name")}}</th>
+        <th data-resizable-column-id="RU Short name">{{e2("RU Short name")}}</th>
+        <th data-resizable-column-id="opt">{{e2("Opt")}}</th>
+    </tr>
+        <form action="?t={{get("t")}}&add" method="post">
+        @csrf
+        <tr class="table-warning">
+            <td>#</td>
+            <td><input type="text" name="en_welding_number" id="" class="form-control"></td>
+            <td><input type="text" name="russian_definition" id="" class="form-control"></td>
+            <td><input type="text" name="iso_4063_definition" id="" class="form-control"></td>
+            <td><input type="text" name="aws" id="" class="form-control"></td>
+            <td><input type="text" name="iso_short_name" id="" class="form-control"></td>
+            <td><input type="text" name="aws_short_name" id="" class="form-control"></td>
+            <td><input type="text" name="ru_short_name" id="" class="form-control"></td>
+            <td><button class="btn btn-outline-success btn-sm"><i class="fa fa-plus"></i></button></td>
+        </tr>
+        </form>
+    </thead>
+    <tbody>
+        <?php 
+        foreach(WeldingMethod::all() AS $weldingMethod) {
+                ?>
+            <tr>
+                <td>{{$weldingMethod->id}}</td>
+                <td><input type="text" name="en_welding_number" value="{{$weldingMethod->en_welding_number}}" table="{{$tableName}}" id="{{$weldingMethod->id}}" class="form-control edit"></td>
+                <td><input type="text" name="russian_definition" value="{{$weldingMethod->russian_definition}}" table="{{$tableName}}" id="{{$weldingMethod->id}}" class="form-control edit"></td>
+                <td><input type="text" name="iso_4063_definition" value="{{$weldingMethod->iso_4063_definition}}" table="{{$tableName}}" id="{{$weldingMethod->id}}" class="form-control edit"></td>
+                <td><input type="text" name="aws" value="{{$weldingMethod->aws}}" table="{{$tableName}}" id="{{$weldingMethod->id}}" class="form-control edit"></td>
+                <td><input type="text" name="iso_short_name" value="{{$weldingMethod->iso_short_name}}" table="{{$tableName}}" id="{{$weldingMethod->id}}" class="form-control edit"></td>
+                <td><input type="text" name="aws_short_name" value="{{$weldingMethod->aws_short_name}}" table="{{$tableName}}" id="{{$weldingMethod->id}}" class="form-control edit"></td>
+                <td><input type="text" name="ru_short_name" value="{{$weldingMethod->ru_short_name}}" table="{{$tableName}}" id="{{$weldingMethod->id}}" class="form-control edit"></td>
+                <td>
+                    <a href="?t={{get("t")}}&delete={{$weldingMethod->id}}" {{delete_teyit()}} class="btn btn-sm btn-outline-danger"><i class="fa fa-times"></i></a>
+                </td>
+            </tr>
+                <?php 
+        }
+        ?>
+    </tbody>
+    
+</table>
+</div>
