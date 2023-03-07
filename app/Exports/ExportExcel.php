0ea6426994771648bb5c523b362888a19f0d43bc
Ümit Tunç <umitreva@gmail.com>
2023-03-07 18:17:16 +0300

    with header system added

--- app/Exports/ExportExcel.php
+++ app/Exports/ExportExcel.php
@@ -3,22 +3,30 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Illuminate\Support\Facades\Schema;
 
-class ExportExcel implements FromCollection
+class ExportExcel implements FromCollection, WithHeadings
 {
 
     public $tableName;
+    public $columnNames;
 
     public function __construct(string $tableName) 
     {
         $this->tableName = $tableName;
+        $this->columnNames = Schema::getColumnListing($this->tableName);
     }
 
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
-    {
+    {   
         return db($this->tableName)->get();
+    }
+    public function headings(): array
+    {
+        return $this->columnNames;
     }
 }
