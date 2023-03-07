e8e045ef9a97811bdbec4f04881ac429815985e9
Ümit Tunç <umitreva@gmail.com>
2023-03-07 09:28:30 +0300

    import excel implements to collection global excel form system has been created.

---
+++ app/Imports/ImportExcel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Imports;
+
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+
+class ImportExcel implements ToCollection
+{
+    public $tableName;
+
+    public function __construct(string $tableName) 
+    {
+        $this->tableName = $tableName;
+    }
+
+    /**
+    * @param Collection $collection
+    */
+    public function collection(Collection $collection)
+    {
+        $firstRow = $collection[0];
+        $otherRow = $collection;
+        unset($otherRow[0]);
+
+        $data = [];
+
+        foreach($otherRow AS $row) {
+            $columnKey = 0;
+            $refactoringRow = [];
+
+            foreach($firstRow AS $column) {
+                $refactoringRow[$column] = $row[$columnKey];
+                $columnKey++;
+            }
+
+            (firstOrUpdate(
+                $refactoringRow, 
+                $this->tableName,
+                [
+                    'id' => $refactoringRow['id']
+                ], false
+            ));
+        }
+    }
+}
