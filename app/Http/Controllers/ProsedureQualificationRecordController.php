1b4b77df70264d8ede191bf48a63e76bdb95c8f1
Ümit Tunç <umitreva@gmail.com>
2023-03-08 21:31:24 +0300

    prosedure_qualification_records mm has been created

---
+++ app/Http/Controllers/ProsedureQualificationRecordController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProsedureQualificationRecord;
+use App\Http\Requests\StoreProsedureQualificationRecordRequest;
+use App\Http\Requests\UpdateProsedureQualificationRecordRequest;
+
+class ProsedureQualificationRecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreProsedureQualificationRecordRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProsedureQualificationRecordRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProsedureQualificationRecord  $prosedureQualificationRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProsedureQualificationRecord $prosedureQualificationRecord)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProsedureQualificationRecord  $prosedureQualificationRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProsedureQualificationRecord $prosedureQualificationRecord)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProsedureQualificationRecordRequest  $request
+     * @param  \App\Models\ProsedureQualificationRecord  $prosedureQualificationRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProsedureQualificationRecordRequest $request, ProsedureQualificationRecord $prosedureQualificationRecord)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProsedureQualificationRecord  $prosedureQualificationRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProsedureQualificationRecord $prosedureQualificationRecord)
+    {
+        //
+    }
+}
