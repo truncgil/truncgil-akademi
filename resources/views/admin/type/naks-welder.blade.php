2c0732a3a4aa9b238f8370af18465ee7e4180212
Ümit Tunç <umitreva@gmail.com>
2023-03-11 13:11:33 +0300

    naks welder blade finished

---
+++ resources/views/admin/type/naks-welder.blade.php
@@ -0,0 +1,34 @@
+<?php 
+
+use App\Models\NaksWelder; 
+
+$title = "Naks Welder";
+$tableWidth="300%";
+$path = "admin.type.naks-welder";
+$listDatas = NaksWelder::orderBy("id","DESC")->paginate(setting('row_count'));
+$tableName = "naks_welders";
+/*
+$relationDatas = [
+    'short_number' => [
+        'table' => 'naks_centers',
+        'value' => 'center_no',
+        'text' => ['center_no'],
+        'type' => 'select',
+        'affected' => [
+            'short_name' => 'center_name',
+        ]
+    ],
+    'welding_method' => [
+        'table' => 'welding_methods',
+        'value' => 'ru_short_name',
+        'text' => ['ru_short_name', 'iso_short_name', 'aws_short_name'],
+        'type' => 'select'
+    ],
+];
+*/
+?>
+<div class="content">
+    <div class="row">   
+        @include("components.blocks.module-block")     
+    </div>
+</div>
