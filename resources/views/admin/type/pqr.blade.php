39cc133f3e68a5e559f4af388e858e74221c91c3
Ümit Tunç <umitreva@gmail.com>
2023-03-22 15:16:16 +0300

    redesign pqr view

--- resources/views/admin/type/pqr.blade.php
+++ resources/views/admin/type/pqr.blade.php
@@ -5,15 +5,17 @@ use App\Models\Material;
 use App\Models\WeldingConsumable; 
 
 $title = "Procedure Qualification Records";
-$tableWidth="500%";
+$tableWidth="800%";
 $listDatas = ProsedureQualificationRecord::orderBy("id","DESC")->paginate(setting('row_count'));
 $tableName = "prosedure_qualification_records";
 $materials = db('materials')->get();
 $weldingPositions = db('welding_positions')->get();
 $weldingConsumables = db('welding_consumables')->get();
+$weldingMethods = db('welding_methods')->whereNotIn("ru_short_name",['*'])->get();
 $currentTypes = db('current_types')->get();
 $jointTypes = db('joint_types')->get();
 $naksTechnology = db('naks_certificates')->get();
+$workTypes = db('work_types')->get();
 
 $relationDatas = [
     'naks_technology' => [
@@ -21,6 +23,21 @@ $relationDatas = [
         'pattern' => '{certificate_no}',
         'type' => 'multiple-choice',
         'seperator' => ' + '
+    ],
+    'base_metal_used_for_pqr_coupon' => [
+        'datas' => $materials,
+        'pattern' => '{steel_grade}',
+        'type' => 'multiple-choice',
+    ],
+    'welding_process' => [
+        'datas' => $weldingMethods,
+        'pattern' => '{ru_short_name}/{en_welding_number}',
+        'type' => 'multiple-choice',
+    ],
+    'welding_position' => [
+        'datas' => $weldingPositions,
+        'pattern' => '{gost}/{en}',
+        'type' => 'multiple-choice'
     ],
     'technology_category' => [
         'filter' => [
@@ -40,21 +57,13 @@ $relationDatas = [
         'pattern' => '{technology_category}',
         'type' => 'select-dropdown'
     ],
-    
-    'base_metal' => [
-        'table' => 'materials',
-        'datas' => $materials,
-        'value' => 'steel_grade',
-        'text' => ['steel_grade'],
-        'type' => 'select',
-        'affected' => []
-    ],
-    
-    'position' => [
+    'welding_method' => [
         'datas' => $weldingPositions,
         'pattern' => '{gost}/{en}',
         'type' => 'multiple-choice'
     ],
+
+    
     'type_grade_1' => [
         'table' => 'materials',
         'datas' => $materials,
@@ -84,58 +93,52 @@ $relationDatas = [
         'text' => ['brend'],
         'type' => 'select',
         'affected' => [
-            'aws_sfa_no_class' => '{aws_class}-{aws_specification}',
-            'gost' => '{gost_class}-{gost_specification}'
+            'filter_metals_aws_sfa_no_class' => '{aws_class}-{aws_specification}',
+            'filter_metals_gost' => '{gost_class}-{gost_specification}'
         ]
     ],
     'current_polarity' => [
-        'table' => 'current_types',
         'datas' => $currentTypes,
-        'value' => 'title',
-        'text' => ['title'],
-        'type' => 'select',
-        'affected' => [
-        ]
+        'pattern' => '{title}',
+        'type' => 'multiple-choice'
     ],
     'joint_design' => [
         'datas' => $jointTypes,
-        'pattern' => '{definition_ru} {definition_en}',
+        'pattern' => '{short_name_en}',
         'type' => 'select-dropdown'
     ],
-    /*
-    'type_grade_1' => [
-        'table' => 'naks_certificates',
-        'value' => 'certificate_no',
-        'text' => ['short_number', 'certificate_no'],
-        'type' => 'select',
-        'affected' => [
-            'technology_category' => 'technology_category',
-        ]
+    'base_metal' => [
+        'datas' => $workTypes,
+        'pattern' => '{title_en}',
+        'type' => 'select-dropdown'
     ],
-    'type_grade_2' => [
-        'table' => 'naks_certificates',
-        'value' => 'certificate_no',
-        'text' => ['short_number', 'certificate_no'],
-        'type' => 'select',
-        'affected' => [
-            'technology_category' => 'technology_category',
-        ]
+    'pwht_temp_range' => [
+        'required' => [
+            'column' => 'pwht',
+            'value' => 'YES'
+        ],
+        'type' => 'integer'
     ],
-    'brend' => [
-        'table' => 'naks_certificates',
-        'value' => 'certificate_no',
-        'text' => ['short_number', 'certificate_no'],
-        'type' => 'select',
-        'affected' => [
-            'technology_category' => 'technology_category',
-        ]
+    'pwht_min_time' => [
+        'required' => [
+            'column' => 'pwht',
+            'value' => 'YES'
+        ],
+        'type' => 'integer'
     ],
-    */
+    
     
    
 ];
 
 ?>
+<script>
+    $(function(){
+        $(".naks_technology input").on("blur", function(){
+            
+        });
+    });
+</script>
 <div class="content">
     <div class="row">   
         @include("components.blocks.module-block")     
