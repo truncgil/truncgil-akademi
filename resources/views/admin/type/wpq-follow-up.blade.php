a85f138d026ee616150e1a9e59baf9cad9e51060
Ümit Tunç <umitreva@gmail.com>
2023-03-28 06:50:47 +0300

    wpq relation changed

--- resources/views/admin/type/wpq-follow-up.blade.php
+++ resources/views/admin/type/wpq-follow-up.blade.php
@@ -7,13 +7,24 @@ $tableWidth="800%";
 $path = "admin.type.wpq-follow-up";
 $listDatas = WelderTest::orderBy("id","DESC")->paginate(setting('row_count'));
 $tableName = "welder_tests";
-
+$naksWelders = db("naks_welders")->get();
 $relationDatas = [
     'naks_no' => [
-        'datas' => db("naks_welders")->get(),
-        'pattern' => '{naks_certificate_no}',
-        'type' => 'multiple-choice',
-        //'seperate' => ' + '
+        'table' => 'naks_welders',
+        'datas' => $naksWelders,
+        'value' => 'naks_certificate_no',
+        'text' => ['naks_certificate_no'],
+        'type' => 'select',
+        'affected' => [
+            'name_surname' => '{welder_name_ru}',
+            'naks_id' => '{welder_id}',
+            'technology_range' => '{group_of_technical_device}',
+            'material_group_gost' => '{material}',
+            'naks_validity' => '{period_of_validity}',
+            'wps_no' => '{wps_no}',
+            'welding_date' => '{wps_date}',
+            'material_group_1' => '{ru_group}',
+        ]
     ],
 ];
 
