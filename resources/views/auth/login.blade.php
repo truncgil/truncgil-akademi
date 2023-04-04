fc8f7b9125a21ed582605e36165fb1835058d8c4
truncgil <umitreva@gmail.com>
2023-04-04 23:11:36 +0300

    added assets

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
             <main id="main-container" style="min-height: 969px;">
 
                 <!-- Page Content -->
-                <div class="bg-image" style="background-image: url('{{url("assets/back.jpg")}}');">
+                <div class="bg-image" style="background-image: url('{{url("assets/img/hero.svg")}}');">
                     <div class="row mx-0 bg-black-op">
                         <div class="hero-static col-md-6 col-xl-8 d-none d-md-flex align-items-md-end">
                             <div class="p-30 js-appear-enabled animated fadeIn" data-toggle="appear">
